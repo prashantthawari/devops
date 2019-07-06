9a3991a1a23f8c3012adea88c227d02e76e6eb5e
root <root@ip-172-31-9-210.ap-south-1.compute.internal>
2019-07-06 05:56:20 +0000

    my first commit

---
+++ shell-scripting/functiondemo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+x="global value"
+
+function bashfunction {
+
+	local x="local value"
+	echo $x
+	}
+echo $x
+
+# call the bash function
+
+bashfunction
+
+echo $x
---
+++ shell-scripting/helloworld.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo 'Hello World'
+myvar="i am learing shell scripting"
+echo $myvar
